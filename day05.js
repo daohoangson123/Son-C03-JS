319a81c8e20747c6d6393b8460048fb908d2efe6
daohoangson123 <daohoangson2109>
2022-09-12 09:50:34 +0700

    123

---
+++ day05.js
@@ -0,0 +1,19 @@
+// let x = 210;
+// if (x % 2 === 0) {
+//     console.log("chan")
+// } else {
+//     console.log("le")
+// }
+
+let diem = 4;
+if (diem < 0 || diem > 10) {
+    console.log("Điểm ko hợp lệ")
+} else if (diem >= 8) {
+    console.log("Giỏi")
+} else if (diem >= 6.5) {
+    console.log("Khá")
+} else if (diem >= 5) {
+    console.log("Trung Bình")
+} else {
+    console.log("Kém")
+}
